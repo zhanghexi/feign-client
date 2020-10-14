2ab190786ba3894d86a8c44c3aa004373260e46d
zhang <zhanghx6@asiainfo.com>
2020-10-14 10:56:47 +0800

    启动类去掉"排除数据库配置的注解参数"

--- src/main/java/com/example/consumer/feign/FeignClientApplication.java
+++ src/main/java/com/example/consumer/feign/FeignClientApplication.java
@@ -2,10 +2,9 @@ package com.example.consumer.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableFeignClients
 public class FeignClientApplication {
 
